002a0ca51eba3ea1cf5c0e12f04825d4c1624399
Cory Butler <corybutler@ymail.com>
2022-08-14 01:10:04 -0500

    update link in footer

--- build/data/sampleCodeFiles.js
+++
@@ -1,6 +0,0 @@
-export default [
-    "absoluteVal.rx",
-    "decToPerc.rx",
-    "factorial.rx",
-    "hypotenuse.rx"
-]
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="footer">
-            <a href="//www.corybutler.com" target="_blank">www.CoryButler.com</a>
+            <a href="http://www.corybutler.com" target="_blank">www.CoryButler.com</a>
             <p>© 2022 Cory Butler</p>
             <p>All Rights Reserved</p>
         </div>
